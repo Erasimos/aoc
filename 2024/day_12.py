f2f89ca7908bef4c251f47dce6e809156e62b1dd
Rasmus Tomasson <rasmus.tomasson@gmail.com>
2024-12-13 00:12:28 +0100

    finished day 12, had one to many beers

--- 2024/day_12.py
+++ 2024/day_12.py
@@ -32,34 +32,101 @@ def get_edge_count(pos: Vec2D, garden: dict):
             count += 1
     return count
 
-def is_corner_pos(garden: dict, pos: Vec2D):
-    
+def get_equal_neighbours(pos: Vec2D, garden: dict):
+    count = 0
     plant_type = garden.get(pos, None)
+    for direction in UDLR:
+        neighbour_pos = pos + direction
+        if garden.get(neighbour_pos, '') == plant_type:
+            count += 1
+    return count
 
-    down_pos = pos + Vec2D(0, 1)
-    up_pos = pos + Vec2D(0, -1)
-    left_pos = pos + Vec2D(-1, 0)
-    right_pos = pos + Vec2D(1, 0)
+def get_sides_pos(garden: dict, pos: Vec2D):
+        
+        plant_type = garden.get(pos, None)
 
-    down_type = garden.get(down_pos, '')
-    up_type = garden.get(up_pos, '')
-    left_type = garden.get(left_pos, '')
-    right_type = garden.get(right_pos, '')
+        n_pos = pos + Vec2D(0, -1)
+        e_pos = pos + Vec2D(1, 0)
+        s_pos = pos + Vec2D(0, 1)
+        w_pos = pos + Vec2D(-1, 0)
 
-    # Top left corner
-    if 
+        n_type = garden.get(n_pos, '')
+        e_type = garden.get(e_pos, '')
+        s_type = garden.get(s_pos, '')
+        w_type = garden.get(w_pos, '')
 
-    # Top right corner
+        nw_pos = pos + Vec2D(-1, -1)
+        ne_pos = pos + Vec2D(1, -1)
+        sw_pos = pos + Vec2D(-1, 1)
+        se_pos = pos + Vec2D(1, 1)
+        
+        nw_type = garden.get(nw_pos, '')
+        ne_type = garden.get(ne_pos, '')
+        sw_type = garden.get(sw_pos, '')
+        se_type = garden.get(se_pos, '')
 
-    # Bottom left corner
+        sides = 0
 
-    # Bottom righ corner
+        ## NW
+        if not nw_type == plant_type:
+
+            if w_type == plant_type and n_type == plant_type:
+                sides += 1
+
+            if not w_type == plant_type and not n_type == plant_type:
+                sides += 1
+
+        elif not n_type == plant_type and not w_type == plant_type:
+                sides += 1
+
+        ## NE
+        if not ne_type == plant_type:
+
+            if n_type == plant_type and e_type == plant_type:
+                sides += 1
+
+            if not n_type == plant_type and not e_type == plant_type:
+                sides += 1
+        
+        elif not n_type == plant_type and not e_type == plant_type:
+                sides += 1
+
+        ## SW
+        if not sw_type == plant_type:
+            
+            if s_type == plant_type and w_type == plant_type:
+                sides += 1
+
+            if not s_type == plant_type and not w_type == plant_type:
+                sides += 1
+
+        elif not s_type == plant_type and not w_type == plant_type:
+                sides += 1
+
+
+        ## SE
+        if not se_type == plant_type:
+            
+            if s_type == plant_type and e_type == plant_type:
+                sides += 1
+
+            if not s_type == plant_type and not e_type == plant_type:
+                sides += 1
+
+        elif not s_type == plant_type and not e_type == plant_type:
+                sides += 1
+
+
+        return sides
 
 def get_sides(garden: dict, region: dict):
+    
     sides = 0
+
     for pos in region.get('positions', []):
-        if is_corner_pos(garden=garden, pos=pos):
-            sides += 1
+           sides += get_sides_pos(garden=garden, pos=pos) 
+
+    return sides
 
 def get_regions(garden: dict):
     regions = []
@@ -117,8 +184,10 @@ def part_two():
 
     garden = get_input()
     regions = get_regions(garden=garden)
+    for region in regions:
+        print(region)
     answer = get_cost_2(regions=regions)
     print_answer(part=2, day=day_nr, answer=answer)
 
 part_one()
-#part_two()
+part_two()
